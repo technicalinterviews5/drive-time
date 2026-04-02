c8e109ea1ded33321860f39eff5748b057e2f0f2
technicalinterviews5 <technicalinterviews5@gmail.com>
2026-04-02 19:16:22 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuration file for Trip Cost app
+const CONFIG = {
+  ORS_API_KEY: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjZlMGI2ZDhjZTE4ZTRlMjI5ZTA1ZDM4MDQyMGRlZjAwIiwiaCI6Im11cm11cjY0In0='
+};
